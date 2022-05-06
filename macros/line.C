fed24e6ea1627e5033e440bb4957e450db282029
f1yToMoon <90560141+f1yToMoon@users.noreply.github.com>
2022-05-06 23:53:25 +0300

    Update line.C

--- macros/line.C
+++ macros/line.C
@@ -1,17 +1,20 @@
-void line(const char* a, const char* q, const char* w) {
-  TFile* f3 = new TFile(a);
-  TTree* t;
-  f3->GetObject("t",t);
-  string o = ":", p = ">>hist";
-  string v = q + o + w + p;
-  const char *l = v.c_str();
-  TCanvas* c2 = new TCanvas("c2", "my 2D plot", 500, 500);
-  t->Draw(l, "", "");
-  TF1 *f1 = new TF1("f1","[0]+[1]*x",0,1);
-  f1->SetParameters(0.,1.);
-  f1->SetLineColor(kRed);
-  hist->Fit(f1);
-  hist->Draw();
-  f1->Draw("same");
-  c2->SaveAs("hist.png");
+void line(const char* a, const char* q, const char* w)
+{
+   TFile* f1 = new TFile(a);
+   TTree* t;
+   f1->GetObject("t",t);
+   string o = ":", p = ">>h2()";
+   string v = q + o + w + p;
+   const char *l = v.c_str();
+   t->Draw(l, "", "goff");
+   
+   TCanvas *c1 = new TCanvas("c1","show profile",600,900);
+   c1->Divide(1,2);
+   c1->cd(1);
+   h2->Draw();
+   c1->cd(2);
+ 
+   TProfile *prof = h2->ProfileX();
+   prof->Fit("pol1");
+
 }
