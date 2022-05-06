a9da2e64588367b409151d17c723a68be2f22ac6
f1yToMoon <90560141+f1yToMoon@users.noreply.github.com>
2022-05-06 20:53:35 +0300

    Update print.C

--- macros/print.C
+++ macros/print.C
@@ -1,1 +1,11 @@
+void print(const char* a, const char* q, const char* w) {
+    TFile* f1 = new TFile(a);
+    TTree* t;
+    f1->GetObject("t",t);
+    string o = ":", p = ">>hist";
+    string v = q + o + w + p;
+    const char *l = v.c_str();
+    TCanvas* c2 = new TCanvas("c2", "my 2D plot", 500, 500);
+    t->Draw(l, "", "");
+ }
 
